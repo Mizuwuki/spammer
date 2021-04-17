46c61b17b30b1894839d4a846f91af8e2ed884cb
Mizuki <82467493+Mizuwuki@users.noreply.github.com>
2021-04-17 11:59:24 -0700

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,7 +1,7 @@
 import pyautogui, time
-time.sleep(20)
+time.sleep(10)
 f = open('filename', 'r')
 for word in f:
   pyautogui.typewrite(word)
   pyautogui.press('Enter')
-  #time.sleep(1)
+  #time.sleep(1.5)
